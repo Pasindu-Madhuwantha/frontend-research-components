849f0a130a08d98e9a1b3868b4beeaf1c87757cb
Pasindu Madhuwantha <73848766+Pasindu-Madhuwantha@users.noreply.github.com>
2024-05-19 16:31:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
       <h1>Harmony Verse</h1>
 
       <section className="home-interface__nav-section">
-        <a href="#" className="common-nav nav_1 nav_3--dark-child">
+        <a href="https://lofi-generator.azurewebsites.net/" className="common-nav nav_1 nav_3--dark-child">
           <div className="dark-bg-theme">
             <span className="content">Lo-Fi Generator</span>
           </div>
         </a>
 
-        <a href="#" className="common-nav nav_2 nav_3--dark-child">
+        <a href="https://emotion-based-music-recommendation.azurewebsites.net/" className="common-nav nav_2 nav_3--dark-child">
           <div className="dark-bg-theme">
             <span className="content">Emotion-based Music Recommendation</span>
           </div>
         </a>
 
-        <a href="#" className="common-nav nav_3 nav_3--dark-child">
+        <a href="https://melody-synthesis.azurewebsites.net/" className="common-nav nav_3 nav_3--dark-child">
           <div className="dark-bg-theme">
             <span className="content">Melody Synthesizer</span>
           </div>
         </a>
 
-        <a href="#" className="common-nav nav_4 nav_3--dark-child">
+        <a href="https://music-genre-classification.azurewebsites.net/" className="common-nav nav_4 nav_3--dark-child">
           <div className="dark-bg-theme">
             <span className="content">Music Genre Classifier</span>
           </div>
